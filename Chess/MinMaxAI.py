b65af7fc990eea887351185a96feebe2479cf452
MarkPepper <mlpepper27@gmail.com>
2022-01-09 12:49:43 +0000

    Extremely slow. Time to lean about alpha beta prun

--- Chess/MinMaxAI.py
+++ Chess/MinMaxAI.py
@@ -22,7 +22,7 @@ class MinMaxAI:
         self.evaluator = Evaluation.Evaluator("simple")
 
 
-    def move(self, board): #THIS WILL BE RENAMED SINCE THIS IS CURRENTLY ONLY A SINGLE DEPTH SEARCH. JUST FOR TESTING.
+    def one_deep_search_score(self, board): #THIS WILL BE RENAMED SINCE THIS IS CURRENTLY ONLY A SINGLE DEPTH SEARCH. JUST FOR TESTING.
         #Performs a one deep minmax search.
         movelist = MoveFinder.move_finder(board)
         colour = board.board[movelist[0][0][1]][movelist[0][0][0]].is_white()
@@ -34,7 +34,8 @@ class MinMaxAI:
             workingboard.move(i[0], i[1])
             workingboard.white_to_move = not workingboard.white_to_move
             if workingboard.check_for_mate():
-                return i
+                scores.append(99999)
+                continue
 
             next_movelist = MoveFinder.move_finder(workingboard)
 
@@ -46,9 +47,55 @@ class MinMaxAI:
                     next_scores.append(-99999 * colour_multiplier)
                 else:
                     next_scores.append(self.evaluator.evaluate_board(next_workingboard.board))
+
+            if min([score * colour_multiplier for score in next_scores]) > 50:
+                return min([score * colour_multiplier for score in next_scores])
             scores.append(min([score * colour_multiplier for score in next_scores]))
-        print(movelist)
-        print(scores)
 
-        return movelist[scores.index(max(scores))]
+        return max(scores)
 
+    def n_deep_search(self, board, n):
+        if (n > 1):
+            #Find AI moves
+            movelist = MoveFinder.move_finder(board)
+            scores = []
+            for i in movelist:
+                workingboard = copy.deepcopy(board)
+                next_scores = []
+                workingboard.move(i[0], i[1])
+                workingboard.white_to_move = not workingboard.white_to_move
+                if workingboard.check_for_mate():
+                    scores.append(99999)
+                    continue
+                elif workingboard.check_for_stalemate():
+                    scores.append(0)
+                
+                next_movelist = MoveFinder.move_finder(workingboard)
+
+                for j in next_movelist:
+                    next_workingboard = copy.deepcopy(workingboard)
+                    next_workingboard.move(j[0],j[1])
+                    next_workingboard.white_to_move = not next_workingboard.white_to_move
+                    if next_workingboard.check_for_mate():
+                        next_scores.append(-99999)
+                    elif next_workingboard.check_for_stalemate():
+                        next_scores.append(0)
+                    else:
+                        next_scores.append(self.n_deep_search(next_workingboard, n-1))
+                if min(next_scores) > 60:
+                    scores.append(min(next_scores))
+                    if n == self.depth:
+                        return movelist[scores.index(min(next_scores))]
+                    else:
+                        return min(next_scores)
+                scores.append(min(next_scores))
+
+            if n == self.depth:
+                return movelist[scores.index(max(scores))]
+            else:
+                return max(scores)
+        else:
+            return self.one_deep_search_score(board) #THIS CODE NOW FAILS IF N=1 OH NO!
+
+    def move(self, board):
+        return self.n_deep_search(board, self.depth)
